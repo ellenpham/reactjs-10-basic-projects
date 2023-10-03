cefe8e20efbf2833b715c0568b292606a7faa9bf
ellenpham <ellenpham1510@gmail.com>
2023-10-03 13:54:22 +1100

    create birthday day buddy project

--- advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState('bob'))
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  return <div>
+    <h4>You clicked {count} times</h4>
+    <button type='button' onClick={handleClick} className="btn">
+      Click me
+    </button>
+  </div>;
 };
 
 export default UseStateBasics;
--- advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    hobby: 'read books',
+  })
+
+  const displayPerson = () => {
+    setPerson({name: 'john', age: 28, hobby: 'play games'});
+  };
+
+  return (
+    <div>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoy: {person.hobby}</h4>
+      <button className='btn' onClick={displayPerson}>Show John</button>
+    </div>
+  );
 };
 
 export default UseStateObject;
