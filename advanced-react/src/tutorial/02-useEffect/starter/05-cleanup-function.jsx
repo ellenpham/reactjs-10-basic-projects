dd3082a9291b71ba5bf5bac749eb1838f45fa104
ellenpham <ellenpham1510@gmail.com>
2023-10-20 14:28:03 +1100

    Cleanup function

--- advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>toggle component</button>
+      {toggle && <RandomComponent />}
+    </div>
+  )
 };
+
+// const RandomComponent = () => {
+
+//   useEffect( () => {
+//     console.log('Hmm, this is interesting')
+//     const intId = setInterval( () => {
+//       console.log("Hello from interval")
+//     }, 1000);
+
+//     return () => {
+//       clearInterval(intId)
+//       console.log('cleanup')
+//     };
+//   }, [])
+//   return <h1>Hello there</h1>
+// }
+
+
+const RandomComponent = () => {
+  useEffect( () => {
+    const someFunc = () => {
+      // some logic
+    }
+
+    window.addEventListener('scroll', someFunc);
+    return () => window.removeEventListener('scroll', someFunc)
+  }, []);
+  return <h1>Hello there</h1>
+}
 
 export default CleanupFunction;
